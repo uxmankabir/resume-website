97325b7ea5e417f9f550f7419bcde3240af6434e
Usman Kabir <usman.kabir.k@gmail.com>
2022-02-26 23:53:08 +0500

    print private ip address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
   end
 
   def client_ip
-    ip=Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+    ip = Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address)
     p '*'*80
-    p "Ip Address: #{ip.ip_address}" if ip
+    p "Ip Address: #{ip}" if ip
     p "Remote IP: #{request.remote_ip}" 
     p '*'*80
     request.remote_ip
