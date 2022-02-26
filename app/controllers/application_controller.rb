4828ff64c01047b56f51b4add058778828003b2f
Usman Kabir <usman.kabir.k@gmail.com>
2022-02-26 23:45:32 +0500

    print IP Address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,11 @@ class ApplicationController < ActionController::Base
   end
 
   def client_ip
+    ip=Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+    p '*'*80
+    p "Ip Address: #{ip.ip_address}" if ip
+    p "Remote IP: #{request.remote_ip}" 
+    p '*'*80
     request.remote_ip
   end
 
