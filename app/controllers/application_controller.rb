b5dd8c094828db97bff7915233975a21fd0c711a
Usman Kabir <usman.kabir.k@gmail.com>
2022-03-01 16:41:24 +0500

    query correctionn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,11 @@ class ApplicationController < ActionController::Base
   private
 
   def monitor_website_visitors
-    Visitor.first_or_create(ip_address: client_ip)
+    Visitor.first_or_create!(ip_address: client_ip)
     @visitors_count = Visitor.count
   end
 
   def client_ip
-    p '*'*80
-    p request.remote_ip
-    p '*'*80
     request.remote_ip
   end
 
