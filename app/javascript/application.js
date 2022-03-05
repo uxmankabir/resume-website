4782c948febfc55edceef992b96680069a175fc3
Usman Kabir <usman.kabir.k@gmail.com>
2022-03-05 22:45:38 +0500

    Follow the cursor design added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,8 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 import "https://kit.fontawesome.com/8fd3d3706e.js"
+
+document.addEventListener("mousemove", function (e) {
+  document.querySelector(".cursor").style.setProperty('--x', e.clientX - 7 + 'px');
+  document.querySelector(".cursor").style.setProperty('--y', e.clientY - 5 + 'px');
+});
