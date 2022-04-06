7d0f0b2b09a62067d612e48d42eb7c34af2e855e
Usman Kabir <usman.kabir.k@gmail.com>
2022-04-06 06:59:30 +0500

    fix scroll snaping issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
 import "controllers"
 
 import "https://kit.fontawesome.com/8fd3d3706e.js"
